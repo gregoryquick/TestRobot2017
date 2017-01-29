1267f379c90bdc7d4c50f9564f474158079e48e2
gregoryquick <gregory@quicks.me>
2017-01-29 10:00:27 -0800

    Code leveling
    Removed most all code so that the project can be re-writen in such a way
    as that the robot class can instantiate

--- src/org/usfirst/frc/team948/robot/Robot.java
+++ src/org/usfirst/frc/team948/robot/Robot.java
@@ -1,56 +1,24 @@
 
 package org.usfirst.frc.team948.robot;
 
-
-import org.usfirst.frc.team948.robot.subsystems.Drive;
-
-import com.kauailabs.navx.frc.AHRS;
-
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
-
-	public static final RobotMap rm = new RobotMap();
-	public static final Drive drive = new Drive();
-	public static OI oi;
 	Command autonomousCommand;
 	SendableChooser chooser;
-//	UsbCamera camera;
 
 	public void robotInit() {
-		oi = new OI();
 		chooser = new SendableChooser();
-		oi.dashboard = NetworkTable.getTable("SmartDashboard");
-		oi.ds = DriverStation.getInstance();
-		rm.navx = new AHRS(SPI.Port.kMXP);
-		rm.preferences = Preferences.getInstance();
-		RobotMap.testEncoder.setDistancePerPulse(1);
 		SmartDashboard.putData("Auto mode", chooser);
-		SmartDashboard.putNumber("DesiredHeading", RobotMap.navx.getAngle());
-//		camera = CameraServer.getInstance().startAutomaticCapture();
-		LiveWindow.addActuator("Drive", "Spark0", RobotMap.testMotor0);
-		LiveWindow.addActuator("Drive", "Spark1", RobotMap.testMotor1);
-		LiveWindow.addActuator("Drive", "Spark2", RobotMap.testMotor2);
-		LiveWindow.addActuator("Drive", "Spark3" , RobotMap.testMotor3);
-		LiveWindow.addSensor("Drive", "Encoder", RobotMap.testEncoder);
 	}
 
 	public void disabledInit() {
-		RobotMap.testEncoder.reset();
-		RobotMap.testEncoder.reset();
+
 	}
 
 	public void disabledPeriodic() {
@@ -77,44 +45,15 @@ public class Robot extends IterativeRobot {
 	
 	public void teleopPeriodic() {
 		periodicAll();
-//		RobotMap.testMotor.set(1);
 		Scheduler.getInstance().run();
 	}
 
 	public void testPeriodic() {
-//		RobotMap.testMotor.set(1);
 		LiveWindow.run();
 	}
 	public void periodicAll()
 	{
-		SmartDashboard.putNumber("Heading", RobotMap.navx.getAngle());
-//		SmartDashboard.putData("reset encoders", new Command(){
-//
-//				@Override
-//				protected void initialize() {
-//					
-//				}
-//	
-//				@Override
-//				protected void execute() {
-//
-//				}
-//	
-//				@Override
-//				protected boolean isFinished() {
-//					return true;
-//				}
-//	
-//				@Override
-//				protected void end() {
-//					
-//				}
-//	
-//				@Override
-//				protected void interrupted() {
-//					end();
-//				}
-//			});
+
 	}
 
 }
--- src/org/usfirst/frc/team948/robot/RobotMap.java
+++ src/org/usfirst/frc/team948/robot/RobotMap.java
@@ -1,13 +1,5 @@
 package org.usfirst.frc.team948.robot;
 
-import com.kauailabs.navx.frc.AHRS;
-
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.Spark;
-
-
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -15,11 +7,5 @@ import edu.wpi.first.wpilibj.Spark;
  * floating around.
  */
 public class RobotMap {
-	public static Spark testMotor0 = new Spark(0);
-	public static Spark testMotor1 = new Spark(1);
-	public static Spark testMotor2 = new Spark(2);
-	public static Spark testMotor3 = new Spark(3);
-	public static Encoder testEncoder = new Encoder(0, 1,false, Encoder.EncodingType.k4X);
-	public static AHRS navx;
-	public Preferences preferences;
+
 }
